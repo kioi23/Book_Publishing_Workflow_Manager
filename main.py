8886feae66284a86f64614d519e35cc688d8b52d
kioi23 <kioijgithinji23@gmail.com>
2026-06-10 13:04:51 +0300

    Refactor main.py by removing unnecessary comments and improving code clarity

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# This is the main entry point for the Book Publishing Workflow Manager CLI tool. It sets up the command-line interface, handles user input, and coordinates interactions between the various components of the application, such as models and utilities.
-import argparse 
-# The argparse module is used to create a command-line interface for the Book Publishing Workflow Manager. It allows the user to specify commands and options when running the application from the terminal. 
+import argparse
+
+from models.author import Author
 
 parser = argparse.ArgumentParser(
     description="Book Publishing Workflow Manager"
-) # Creates an ArgumentParser object with a description of the application. This object will be used to define the command-line arguments and options for the application.
+)
 
 subparsers = parser.add_subparsers(dest="command")
 
-# add-author command
 author_parser = subparsers.add_parser(
     "add-author",
     help="Add a new author"
@@ -27,15 +26,6 @@ author_parser.add_argument(
 args = parser.parse_args()
 
 if args.command == "add-author":
-    print(
-        f"Author Created: {args.name} ({args.email})"
-    )
-
-if args.command == "add-author":
-
-  from models.author import Author
-
-if args.command == "add-author":
 
     author = Author(
         args.name,
@@ -43,3 +33,4 @@ if args.command == "add-author":
     )
 
     print(author)
+    
