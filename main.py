2fa3734656edd45a8bab0f9af5544a185e12a124
kioi23 <kioijgithinji23@gmail.com>
2026-06-10 11:24:27 +0300

    added cli functions to main.py file

--- main.py
+++ main.py
@@ -2,14 +2,33 @@
 import argparse 
 # The argparse module is used to create a command-line interface for the Book Publishing Workflow Manager. It allows the user to specify commands and options when running the application from the terminal. 
 
-from models.book import Book
-from models.author import Author
-from models.editor import Editor
-from models.task import Task
-from utils.storage import save_data, load_data 
-
-
 parser = argparse.ArgumentParser(
     description="Book Publishing Workflow Manager"
 ) # Creates an ArgumentParser object with a description of the application. This object will be used to define the command-line arguments and options for the application.
+
+subparsers = parser.add_subparsers(dest="command")
+
+# add-author command
+author_parser = subparsers.add_parser(
+    "add-author",
+    help="Add a new author"
+)
+
+author_parser.add_argument(
+    "--name",
+    required=True
+)
+
+author_parser.add_argument(
+    "--email",
+    required=True
+)
+
+args = parser.parse_args()
+
+if args.command == "add-author":
+    print(
+        f"Author Created: {args.name} ({args.email})"
+    )
+
 
