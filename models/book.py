1623cb97f3d346d9bf48a1e450e3bb17f23afeb3
kioi23 <kioijgithinji23@gmail.com>
2026-06-12 08:32:59 +0300

    removing redundant comments and improving code readability from Book class

--- models/book.py
+++ models/book.py
@@ -1,29 +1,22 @@
-# This module defines the Book class, which represents a book in the book publishing workflow manager. The Book class includes attributes such as title, genre, author, editor, tasks, and milestone, as well as a unique ID for each book.
 class Book:
 
-    id_counter = 1 # Class variable to keep track of the next available ID for books.
-    
-    # Initializes a Book object with a title, genre, and author. It assigns a unique ID to the book and initializes the editor, tasks, and milestone attributes.
+    id_counter = 1
+
     def __init__(self, title, genre, author):
         self.id = Book.id_counter
         Book.id_counter += 1
 
-        # Initializes the title, genre, and author attributes of the Book object.
         self.title = title
         self.genre = genre
         self.author = author
 
         self.editor = None
-
         self.tasks = []
-
         self.milestone = "Draft"
-    # String representation of the Book object.
-
 
     def __str__(self):
         return (
             f"Book ID: {self.id} | "
             f"Title: {self.title} | "
             f"Milestone: {self.milestone}"
-    )
+        )
